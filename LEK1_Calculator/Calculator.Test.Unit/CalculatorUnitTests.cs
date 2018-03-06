b1ea0e3506060d737b226e2481ede5d00af0c360
Soeren Bech <soeren-bechy7@hotmail.com>
2018-03-06 09:32:27 +0100

    Ny testcase

--- LEK1_Calculator/Calculator.Test.Unit/CalculatorUnitTests.cs
+++ LEK1_Calculator/Calculator.Test.Unit/CalculatorUnitTests.cs
@@ -21,6 +21,7 @@ namespace Calculator.Test.Unit
         [TestCase(-3, -2, -5)]
         [TestCase(-3, 2, -1)]
         [TestCase(3, -2, 1)]
+        [TestCase(3, -2, 1)]
         public void Add_AddPosAndNegNumbers_ResultIsCorrect(int a, int b, int result)
         {
             Assert.That(_uut.Add(a, b), Is.EqualTo(result));
