4108aa0c7f96a0891c0960b7f2ab310cf6c9245b
Soeren Bech <soeren-bechy7@hotmail.com>
2018-03-15 16:16:27 +0100

    Redigerede Koden

---
+++ Handin2/MicrowaveOven(Teacher)/MicrowaveOven.Test.Integration/IT1_Display.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace MicrowaveOven.Test.Integration
+{
+    [TestFixture]
+    public class IT1_Display
+    {
+        private Display uut;
+        private IOutput output;
+
+        [SetUp]
+        public void Setup()
+        {
+            output = Substitute.For<IOutput>();
+            uut = new Display(output);
+        }
+
+        [Test]
+        public void ShowTime_ZeroMinuteZeroSeconds_CorrectOutput()
+        {
+            uut.ShowTime(0, 0);
+            output.Received().OutputLine(Arg.Is<string>(str => str.Contains("00:00")));
+        }
+
+        [Test]
+        public void ShowTime_SomeMinuteSomeSecond_CorrectOutput()
+        {
+            uut.ShowTime(10, 15);
+            output.Received().OutputLine(Arg.Is<string>(str => str.Contains("10:15")));
+        }
+
+        [Test]
+        public void ShowPower_Zero_CorrectOutput()
+        {
+            uut.ShowPower(0);
+            output.Received().OutputLine(Arg.Is<string>(str => str.Contains("0 W")));
+        }
+
+        [Test]
+        public void ShowPower_NotZero_CorrectOutput()
+        {
+            uut.ShowPower(150);
+            output.Received().OutputLine(Arg.Is<string>(str => str.Contains("150 W")));
+        }
+
+        [Test]
+        public void Clear_CorrectOutput()
+        {
+            uut.Clear();
+            output.Received().OutputLine(Arg.Is<string>(str => str.Contains("cleared")));
+        }
+    }
+}
---
+++ Handin2/MicrowaveOven(Teacher)/MicrowaveOven.Test.Integration/IT2_PowerTube.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace MicrowaveOven.Test.Integration
+{
+    [TestFixture]
+    class IT2_PowerTube
+    {
+        private PowerTube uut;
+        private IOutput output;
+
+        [SetUp]
+        public void Setup()
+        {
+            output = Substitute.For<IOutput>();
+            uut = new PowerTube(output);
+        }
+
+        [Test]
+        public void TurnOn_WasOff_CorrectOutput()
+        {
+            uut.TurnOn(50);
+            output.Received().OutputLine(Arg.Is<string>(str => str.Contains("50 %")));
+        }
+
+        [Test]
+        public void TurnOff_WasOn_CorrectOutput()
+        {
+            uut.TurnOn(50);
+            uut.TurnOff();
+            output.Received().OutputLine(Arg.Is<string>(str => str.Contains("off")));
+        }
+
+        [Test]
+        public void TurnOff_WasOff_NoOutput()
+        {
+            uut.TurnOff();
+            output.DidNotReceive().OutputLine(Arg.Any<string>());
+        }
+
+        [Test]
+        public void TurnOn_WasOn_ThrowsException()
+        {
+            uut.TurnOn(50);
+            Assert.Throws<System.ApplicationException>(() => uut.TurnOn(60));
+        }
+
+        [Test]
+        public void TurnOn_NegativePower_ThrowsException()
+        {
+            Assert.Throws<System.ArgumentOutOfRangeException>(() => uut.TurnOn(-1));
+        }
+
+        [Test]
+        public void TurnOn_HighPower_ThrowsException()
+        {
+            Assert.Throws<System.ArgumentOutOfRangeException>(() => uut.TurnOn(101));
+        }
+
+        [Test]
+        public void TurnOn_ZeroPower_ThrowsException()
+        {
+            Assert.Throws<System.ArgumentOutOfRangeException>(() => uut.TurnOn(0));
+        }
+    }
+}
